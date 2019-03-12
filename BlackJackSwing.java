0b94eb2026d35adfa1022cbbf5631d86c2c1ea93
OkaGouhei <g.oka1975@gmail.com>
2019-03-12 13:18:21 +0900

    トランプ　ボタン　表示

--- BlackJackSwing.java
+++ BlackJackSwing.java
@@ -8,10 +8,17 @@ import java.awt.Container;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseAdapter;
+
+import javax.swing.JButton;
+
+
 class BlackJackSwing extends JFrame{
   public static void main(String args[]){
     BlackJackSwing frame = new BlackJackSwing("タイトル");
     frame.setVisible(true);
+
   }
 
   BlackJackSwing(String title){
@@ -24,10 +31,24 @@ class BlackJackSwing extends JFrame{
     JLabel label = new JLabel(number+" heart");
     label.setPreferredSize(new Dimension(90,120));
     label.setBorder(new LineBorder(Color.BLUE, 3, true));
-
     p.add(label);
-
     Container contentPane = getContentPane();
     contentPane.add(p, BorderLayout.CENTER);
+
+
+    JPanel q = new JPanel();
+    JButton button = new JButton("bet");
+    q.add(button);
+    Container contentButton = getContentPane();
+    contentButton.add(q, BorderLayout.SOUTH);
+
+    button.addMouseListener(
+      new MouseAdapter(){
+        public void mouseClicked(MouseEvent  event){
+          button.doClick(500);
+        }
+      }
+    );
+
   }
 }
